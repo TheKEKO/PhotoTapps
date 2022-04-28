9104315adefc8632eba5463e3da4b035c791771b
Aleksandr F <a7214370@gmail.com>
2022-04-28 14:10:42 +0300

    Completed Lesson № 15

--- PhotoTapps/ViewControllers/PhotosCollectionViewController.swift
+++ PhotoTapps/ViewControllers/PhotosCollectionViewController.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    
+    let itemsPerRow: CGFloat = 2 // определяет количество ячеек на одном ряду
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20) // Метод создает отступы от границ экрана
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        // 2-ый способ настройки Layout Flow через код
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.itemSize = CGSize(width: 70, height: 70) // определение размеров ячеек
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20) // создает отступы от границ экрана
+//        layout.minimumLineSpacing = 20 // настраивают расстояние по рядам и линиям
+//        layout.minimumInteritemSpacing = 20 // настраивают расстояние по рядам и линиям
+//        layout.scrollDirection = .vertical // перемещение по горизонтали или вертикали
+//
+//        collectionView.showsVerticalScrollIndicator = false // отключает полоску перемещения
     }
     
     // MARK: UICollectionViewDataSource
@@ -33,33 +48,37 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
 }
 
+// 1-ый способ настройки Layout Flow через код
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
-    
+
     //    // Метод для определения размеров ячеек
     //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     //        return CGSize(width: 100, height: 50)
     //    }
-    
+
     // Метод для определения количества ячеек на одном ряду и размеры
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 2 // определяет количество ячеек на одном ряду
-        let paddingWidh = 20 * (itemsPerRow + 1) // количество отступов на одном ряду
-        let availabaleWidth = collectionView.frame.width - paddingWidh // получаем доступную ширину
-        let widthPerItem = availabaleWidth / itemsPerRow // получаем точную доступную ширину для одного обьекта
+//        let itemsPerRow: CGFloat = 2 // определяет количество ячеек на одном ряду
+
+        let paddingWidth = 20 * (itemsPerRow + 1) // количество отступов на одном ряду
+        let availabaleWidth = collectionView.frame.width - paddingWidth // получаем доступную ширину
+        let widthPerItem = availabaleWidth / itemsPerRow // получаем точную доступную ширину для одного объекта
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-    
+
     // Метод создает отступы от границ экрана
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        return sectionInserts
     }
-    
+
     // Методы настраивают расстояние по рядам и линиям
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+//        return 20
+        return sectionInserts.left
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+//        return 20
+        return sectionInserts.left
     }
 }
